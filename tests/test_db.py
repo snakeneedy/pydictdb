7fab8925b8e19e2573641f5a87812b4fa90b614a
Pin-Xuan She <snakeneedy@gmail.com>
2019-11-20 08:43:03 +0800

    ✅ test: rename class name in tests/test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,21 +3,21 @@ import unittest
 from pydictdb import db
 
 
-class User(db.Model):
+class ModelInTestDB(db.Model):
     pass
 
 
 class ModelTestCase(unittest.TestCase):
     def test_put(self):
-        model = User(name='Sam', score=90)
+        model = ModelInTestDB(name='Sam', score=90)
         key = model.put()
-        self.assertTrue('User' in db._database_in_use._tables)
+        self.assertTrue('ModelInTestDB' in db._database_in_use._tables)
         self.assertEqual(db._database_in_use.table(key.kind).get(key.object_id),
                 model.to_dict(exclude=('key',)))
 
 
 class KeyTestCase(unittest.TestCase):
     def test_get_class(self):
-        self.assertEqual(db.Key._get_class('User'), User)
+        self.assertEqual(db.Key._get_class('ModelInTestDB'), ModelInTestDB)
         with self.assertRaises(TypeError):
             db.Key._get_class('abcdefghijklmnopqrstuvwxyz')
